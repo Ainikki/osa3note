d0fb0fe0821bef06dd9e3ef0648ff6df0dbdefd5
Ainikki <ainikki@users.noreply.github.com>
2024-04-20 15:06:47 +0300

    lisätty portti

--- index.js
+++ index.js
@@ -92,7 +92,8 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
